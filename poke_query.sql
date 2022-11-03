de1df535925d49cbd602dd50f110f58d4cf2fdc2
Noone <Noone>
2022-11-03 12:34:30 +0530

    query

---
+++ poke_query.sql
@@ -0,0 +1,4 @@
+SELECT id as 'Pokedex No',english as Name,type,HP,Attack,Defense,Sp_Attack as 'Special Attack',Sp_Defense as 'Special Defence',Speed FROM pokedex order by id desc;
+
+SELECT id as 'Pokdex No',english as Name,type,HP,Attack,Defense,Sp_Attack as 'Special Attack',Sp_Defense as 'Special Defence',Speed FROM pokedex;
+
